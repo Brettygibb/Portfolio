90af7918ff29e9d3e6e33eede35bbd3612ccf083
Brett Gibbons <BGIBBONS01@mynbcc.ca>
2024-09-29 10:10:36 -0300

    About

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ function App() {
       <header className="App-header">
         <h1>Brett Gibbons Portfolio</h1>
         <p>Welcome to my portfolio! Check out my projects below.</p>
-        <p>this is a test</p>
-        <p>test 2</p>
+
         {/* Add more components or sections here */}
         <About />
       </header>
